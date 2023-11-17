c7eb8c6566a20b6dc32086b9c35ec5aa127d9ed3
WarrenHawker <hawker.warren@gmail.com>
2023-11-17 22:39:43 +0000

    brought back framer animations for page transitions

--- src/components/AnimatedRoutes.tsx
+++ src/components/AnimatedRoutes.tsx
@@ -1,4 +1,4 @@
-// import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import About from '../views/About';
 import Home from '../views/Home';
@@ -7,16 +7,15 @@ import Projects from '../views/Projects';
 const AnimatedRoutes = () => {
   const location = useLocation();
   return (
-    // <AnimatePresence mode={'wait'}>
-    <Routes key={location.pathname} location={location}>
-      <Route>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-      </Route>
-    </Routes>
-    //{' '}
-    // </AnimatePresence>
+    <AnimatePresence mode={'wait'}>
+      <Routes key={location.pathname} location={location}>
+        <Route>
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+        </Route>
+      </Routes>
+    </AnimatePresence>
   );
 };
 
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,24 +1,21 @@
 import { PropsWithChildren } from 'react';
 import Header from './Header';
 import Footer from './Footer';
-// import { LazyMotion, domAnimation, m } from 'framer-motion';
+import { LazyMotion, domAnimation, m } from 'framer-motion';
 
 const Layout = ({ children }: PropsWithChildren) => {
-  // const isMobile = window.innerWidth < 600;
-
-  // const MotionComponent = isMobile ? 'main' : m.main;
   return (
     <div className="page-content">
       <Header />
-      {/* <LazyMotion features={domAnimation} strict> */}
-      <main
-      // initial={{ x: 300, opacity: 0 }}
-      // animate={{ x: 0, opacity: 1 }}
-      // exit={{ x: -300, opacity: 0 }}
-      >
-        {children}
-      </main>
-      {/* </LazyMotion> */}
+      <LazyMotion features={domAnimation} strict>
+        <m.main
+          initial={{ x: 300, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          exit={{ x: -300, opacity: 0 }}
+        >
+          {children}
+        </m.main>
+      </LazyMotion>
       <Footer />
     </div>
   );
