4d8a866dded964995ee2c78962fe6e534c9256ec
WarrenHawker <hawker.warren@gmail.com>
2023-11-17 21:05:23 +0000

    test with framer disabled on mobile devices

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,17 +4,26 @@ import Footer from './Footer';
 import { LazyMotion, domAnimation, m } from 'framer-motion';
 
 const Layout = ({ children }: PropsWithChildren) => {
+  const isMobile = window.innerWidth < 600;
+
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
+  const MotionComponent = isMobile ? 'div' : m.main;
   return (
     <div className="page-content">
       <Header />
       <LazyMotion features={domAnimation} strict>
-        <m.main
+        <MotionComponent
           initial={{ x: 300, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           exit={{ x: -300, opacity: 0 }}
+          variants={variants}
         >
           {children}
-        </m.main>
+        </MotionComponent>
       </LazyMotion>
       <Footer />
     </div>
